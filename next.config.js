f9c21d4c4dde3459c2ffed7ca6bafa2a2e86ad09
minlaxz <minminlaxz@gmail.com>
2021-11-05 23:46:41 +0630

    getStaticProps and getStaticPaths

--- next.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    // basePath: '/74657374-next',
-}
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
 
-export default function Home() {
+
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
             <Head>
@@ -21,9 +24,35 @@ export default function Home() {
                     </a>
                 </p>
             </section>
-            <Link href="posts/first-post">
-                <a>Read this{''}&rarr;</a>
-            </Link> blog post.
+
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                {/* 2020-01-02 => January 2, 2020 */}
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    // [{ id, date, title }, { id, date, title }]
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
